bf017af54b8a7f864417ec9d6b1e8c4f9185363e
RodrigoArias02 <ezequielcomar86@gmail.com>
2026-05-21 14:08:32 -0300

    ultima correcion de ruta

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { CartProvider } from "./components/cart/cartProvider.jsx"
+import { CartProvider } from "./components/cart/cartprovider.jsx"
 import './index.css'
 import App from './App.jsx'
 
